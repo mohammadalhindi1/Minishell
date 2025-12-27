5df2cd965a325728803a4681bf5800cb0239ac86
omar alethamat <omaralethamat@gmail.com>
2025-12-27 15:18:37 +0300

    Add files via upload

---
+++ omar_update/src/execution/execute_pipeline.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_pipeline.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malhendi <malhendi@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/21 13:17:34 by malhendi          #+#    #+#             */
+/*   Updated: 2025/12/21 13:17:34 by malhendi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	wait_last_status(pid_t *pids, int n)
+{
+	int		i;
+	int		status;
+	int		last_status;
+	pid_t	last_pid;
+
+	i = 0;
+	last_status = 0;
+	last_pid = pids[n - 1];
+	while (i < n)
+	{
+		if (waitpid(pids[i], &status, 0) > 0)
+		{
+			if (pids[i] == last_pid)
+				last_status = status;
+		}
+		i++;
+	}
+	if (WIFEXITED(last_status))
+		g_exit_status = WEXITSTATUS(last_status);
+	else if (WIFSIGNALED(last_status))
+		g_exit_status = 128 + WTERMSIG(last_status);
+	return (g_exit_status);
+}
+
+int	execute_pipeline(t_cmd *cmds, int n, char **envp)
+{
+	t_exec_ctx	x;
+
+	if (!cmds || n <= 0)
+		return (0);
+	x.n = n;
+	x.envp = envp;
+	x.prev_read = -1;
+	if (prepare_heredocs(cmds, n, x.hd_fds))
+		return (g_exit_status);
+	x.i = 0;
+	while (x.i < n)
+	{
+		if (spawn_cmd(cmds, &x))
+			return (1);
+		x.i++;
+	}
+	close_fd(x.prev_read);
+	x.i = 0;
+	while (x.i < n)
+		close_fd(x.hd_fds[x.i++]);
+	return (wait_last_status(x.pids, n));
+}
