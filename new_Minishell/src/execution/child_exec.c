4986bba369fff41a305020e9d95eb48ec2db3f2b
omar alethamat <omaralethamat@gmail.com>
2026-01-14 22:56:59 +0300

    Add files via upload

---
+++ new_Minishell/src/execution/child_exec.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   child_exec.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malhendi <malhendi@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 13:48:23 by malhendi          #+#    #+#             */
+/*   Updated: 2025/12/23 13:48:23 by malhendi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <errno.h>
+
+static void	exec_with_slash(t_cmd *cmd, char **envp)
+{
+	execve(cmd->args[0], cmd->args, envp);
+	minishell_perror(cmd->args[0]);
+	if (errno == ENOENT)
+		exit(127);
+	exit(126);
+}
+
+// omar add
+
+static void	free_args(char **args)
+{
+	int	i;
+
+	if (!args)
+		return ;
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
+
+static void	free_cmd(t_cmd *cmd)
+{
+	if (!cmd)
+		return ;
+	free_args(cmd->args);
+	free(cmd->infile);
+	free(cmd->outfile);
+	free(cmd->heredoc);
+	free(cmd);
+}
+
+// omar add
+
+static void	exec_with_path(t_cmd *cmd, char **envp)
+{
+	char	*path;
+
+	path = find_exec_path(cmd->args[0], envp);
+	if (!path)
+	{
+		minishell_cmd_not_found(cmd->args[0]);
+		free_cmd(cmd);
+		free_args(envp);
+		exit(127);
+	}
+	execve(path, cmd->args, envp);
+	minishell_perror("execve");
+	free(path);
+	exit(126);
+}
+
+void	exec_external(t_cmd *cmd, char **envp)
+{
+	if (!cmd || !cmd->args || !cmd->args[0])
+		exit(0);
+	if (ft_strchr(cmd->args[0], '/'))
+		exec_with_slash(cmd, envp);
+	exec_with_path(cmd, envp);
+}
---
+++ new_Minishell/src/execution/execute_pipeline_utils.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_pipeline_utils.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malhendi <malhendi@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 13:49:33 by malhendi          #+#    #+#             */
+/*   Updated: 2025/12/23 13:49:33 by malhendi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	pipe_if_needed(int idx, int n, int pipe_fd[2])
+{
+	pipe_fd[0] = -1;
+	pipe_fd[1] = -1;
+	if (idx < n - 1)
+	{
+		if (pipe(pipe_fd) == -1)
+		{
+			minishell_perror("pipe");
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	prepare_heredocs(t_cmd *cmds, int n, int *hd_fds)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		hd_fds[i] = -1;
+		if (cmds[i].heredoc)
+		{
+			hd_fds[i] = open_heredoc_fd(cmds[i].heredoc);
+			if (hd_fds[i] < 0)
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+static void	spawn_child(t_cmd *cmds, t_exec_ctx *x)
+{
+	t_child_ctx	c;
+
+	c.idx = x->i;
+	c.n = x->n;
+	c.prev_read = x->prev_read;
+	c.pipe_fd[0] = x->pipe_fd[0];
+	c.pipe_fd[1] = x->pipe_fd[1];
+	c.envp = x->envp;
+	c.heredoc_fd = x->hd_fds[x->i];
+	child_run(&cmds[x->i], &c);
+}
+
+int	spawn_cmd(t_cmd *cmds, t_exec_ctx *x)
+{
+	if (pipe_if_needed(x->i, x->n, x->pipe_fd))
+		return (1);
+	x->pids[x->i] = fork();
+	if (x->pids[x->i] == -1)
+	{
+		minishell_perror("fork");
+		return (1);
+	}
+	if (x->pids[x->i] == 0)
+		spawn_child(cmds, x);
+	close_fd(x->prev_read);
+	if (x->i < x->n - 1)
+	{
+		close_fd(x->pipe_fd[1]);
+		x->prev_read = x->pipe_fd[0];
+	}
+	return (0);
+}
