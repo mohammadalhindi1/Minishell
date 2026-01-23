d397762fbab7d7d24b4072ddbed3a2156260cd22
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2026-01-23 20:24:23 +0300

    Add files via upload

---
+++ Minishell/src/parsing/parse_helper.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_helper.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malhendi <malhendi@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/22 11:58:09 by malhendi          #+#    #+#             */
+/*   Updated: 2026/01/22 00:00:00 by malhendi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_op(char *t)
+{
+	if (!t)
+		return (0);
+	if (!ft_strncmp(t, "|", 2))
+		return (1);
+	if (!ft_strncmp(t, "<", 2) || !ft_strncmp(t, ">", 2))
+		return (1);
+	if (!ft_strncmp(t, "<<", 3) || !ft_strncmp(t, ">>", 3))
+		return (1);
+	return (0);
+}
+
+void	free_tokens(char **t)
+{
+	int	i;
+
+	if (!t)
+		return ;
+	i = 0;
+	while (t[i])
+	{
+		free(t[i]);
+		i++;
+	}
+	free(t);
+}
+
+void	free_cmds_partial(t_cmd *cmds, int n)
+{
+	int	i;
+
+	if (!cmds)
+		return ;
+	i = 0;
+	while (i < n)
+	{
+		free_tokens(cmds[i].args);
+		free(cmds[i].infile);
+		free(cmds[i].outfile);
+		free(cmds[i].heredoc);
+		i++;
+	}
+	free(cmds);
+}
+
+int	count_cmds(char **t)
+{
+	int	i;
+	int	c;
+
+	i = 0;
+	c = 1;
+	while (t && t[i])
+	{
+		if (!ft_strncmp(t[i], "|", 2))
+			c++;
+		i++;
+	}
+	return (c);
+}
---
+++ Minishell/src/parsing/token_helper.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_helper.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malhendi <malhendi@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 19:52:43 by malhendi          #+#    #+#             */
+/*   Updated: 2026/01/08 19:52:43 by malhendi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	add_if_word(t_lexer *lx, int start, int end)
+{
+	t_range	r;
+
+	if (end > start)
+	{
+		r.start = start;
+		r.end = end;
+		add_word_to_token(lx->tokens, lx->words, lx->line, r);
+	}
+}
+
+void	scan_word(char *line, int *i)
+{
+	char	q;
+
+	while (line[*i] && line[*i] != ' ' && line[*i] != '|'
+		&& line[*i] != '<' && line[*i] != '>')
+	{
+		if (line[*i] == '"' || line[*i] == 39)
+		{
+			q = line[*i];
+			(*i)++;
+			while (line[*i] && line[*i] != q)
+				(*i)++;
+			if (line[*i] == q)
+				(*i)++;
+		}
+		else
+			(*i)++;
+	}
+}
+
+void	skip_spaces(char *line, int *i, int *start)
+{
+	while (line[*i] == ' ')
+	{
+		(*i)++;
+		*start = *i;
+	}
+}
+
+void	handle_quotes(t_lexer *lx, int *i, int *start)
+{
+	int		j;
+	t_range	r;
+
+	j = 1;
+	while (lx->line[*i + j] != lx->line[*i])
+		j++;
+	r.start = *i;
+	r.end = *i + j + 1;
+	add_word_to_token(lx->tokens, lx->words, lx->line, r);
+	*i += j + 1;
+	*start = *i;
+}
+
+char	**build_final_tokens(char **tokens, int words)
+{
+	int		i;
+	char	**final;
+
+	final = malloc(sizeof(char *) * (words + 1));
+	if (!final)
+		return (NULL);
+	i = 0;
+	while (i < words)
+	{
+		final[i] = tokens[i];
+		i++;
+	}
+	final[words] = NULL;
+	free(tokens);
+	return (final);
+}
+/*
+** Omar note:
+** scan_word now skips quoted parts instead of stopping on quotes.
+** This allows concatenated words like l""s or "l"s to be parsed correctly.
+*/
